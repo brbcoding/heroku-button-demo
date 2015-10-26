5c1a1ec66e0a493c2a7a19431bb96ab8616be02c
Cody Henshaw <cghensha@gmail.com>
2015-10-26 16:50:39 -0700

    added express

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/api', function (req, res) {
+  var firstName = process.env.FIRST_NAME || 'noname';
+  var lastName = process.env.LAST_NAME || 'noname';
+  var name = { firstName: firstName, lastName: lastName };
+  res.json(name);
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
